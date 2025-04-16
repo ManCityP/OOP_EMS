c03e8a3e96daa256d3b98f0359060ee32b6554c5
ManCityP <mohamed.a.eltaie@gmail.com>
2025-04-16 11:03:13 +0200

    updated packages

--- src/main/java/p1/Admin.java
+++ src/main/java/p1/Admin.java
@@ -1,9 +1,23 @@
 package p1;
 
 import p3.User;
+import p3.Wallet;
 
 import java.util.Date;
 
 public class Admin extends User {
+    public Admin(String username, String password, Date dob, Gender gender) {
 
+    }
+    public static void AddCategory(String category) throws Exception {
+        if(Category.categories.contains(category))
+            throw new Exception("This Category already exists!");
+        Category.categories.add(category);
+    }
+
+    public static void RemoveCategory(String category) throws Exception {
+        if (!Category.categories.contains(category))
+            throw new Exception("This Category doesn't exist!");
+        Category.categories.remove(category);
+    }
 }
--- src/main/java/p1/Category.java
+++ src/main/java/p1/Category.java
@@ -12,26 +12,11 @@ public class Category {
     public Category(String category) throws Exception {
         if (!categories.contains(category))
             throw new Exception("This Category doesn't exist!");
-        else
-           this.category = category;
+        this.category = category;
     }
 
     @Override
     public String toString() {
         return this.category;
-    }
-
-    static void AddCategory(String category) throws Exception {
-        if(categories.contains(category))
-            throw new Exception("This Category already exists!");
-        else
-            categories.add(category);
-    }
-
-    static void RemoveCategory(String category) throws Exception {
-        if (!categories.contains(category))
-            throw new Exception("This Category doesn't exist!");
-        else
-            categories.remove(category);
     }
 }
