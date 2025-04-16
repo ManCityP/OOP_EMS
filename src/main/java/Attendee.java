2a2c76a688732be00e38656d13253bb59174d369
Fris233 <baefris@gmail.com>
2025-04-16 10:45:33 +0200

    ARESSSSSSSSSSS

--- src/main/java/Attendee.java
+++ src/main/java/Attendee.java
@@ -1,2 +1,35 @@
-public class Attendee {
+import p1.Category;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Attendee extends User {
+ // static int numberOfAttendees;
+    // String address;
+    ArrayList<Category> interests = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+    Attendee(){
+        super();
+        //  numberOfAttendees++;
+    }
+                                                                                 // String Address excluded in constructor
+                                                                                  // String address
+Attendee(String username, String password, Date dob, Wallet wallet, Gender gender, ArrayList<Category> interests) {
+super(username,password,dob, wallet, gender);
+this.interests = interests;
+//this.address=address;
+    //  numberOfAttendees++;
+
+}
+
+
 }
