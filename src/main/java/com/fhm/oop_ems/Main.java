048afd75fc7991b4d5ad7ce5e97038ba25114784
ManCityP <mohamed.a.eltaie@gmail.com>
2025-04-16 12:37:56 +0200

    More

--- src/main/java/com/fhm/oop_ems/Main.java
+++ src/main/java/com/fhm/oop_ems/Main.java
@@ -9,9 +9,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws Exception {
         Map<Day, TimeRange> map = new LinkedHashMap<>();
-        //map.put(Day.FRIDAY, new TimeRange(9, 23.5));
         map.put(Day.SATURDAY, new TimeRange(6.25, 17));
-        //map.put(Day.SUNDAY, new TimeRange(18, 17));
         map.put(Day.MONDAY, new TimeRange(9, 21.57));
         map.put(Day.TUESDAY, new TimeRange("9:11", "19:12"));
         map.put(Day.WEDNESDAY, new TimeRange("9:30", "17:15"));
