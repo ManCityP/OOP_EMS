f16976da4278020a0ecbe226f7b848d93fc4085f
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-04-16 10:53:42 +0200

    fuck you ya mohy

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -1,9 +1,21 @@
 import p2.Event;
+import p2.Organizer;
 
 public class Wallet {
     double balance;
+    int walletNumber;
+    Wallet(){}
+    Wallet(double balance,int walletNumber){
+        this.balance = balance;
+        this.walletNumber = walletNumber;
+    }
 
-    void TransferMoney(Event event){
+    void PurchaseEvent(Event event, Organizer organizer) throws Exception{
+        if(this.balance < event.GetPrice())
+            throw new Exception("Invalid balance");
+        else{
+            this.balance -= event.GetPrice();
 
+        }
     }
 }
