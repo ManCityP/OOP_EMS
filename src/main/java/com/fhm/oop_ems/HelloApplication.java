85711afc0d572f6a5c87049ff6d4f09df832dab2
ManCityP <mohamed.a.eltaie@gmail.com>
2025-04-16 09:39:06 +0200

    Create Classes

--- src/main/java/com/fhm/oop_ems/HelloApplication.java
+++ src/main/java/com/fhm/oop_ems/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-//Test Commit.
+//Test Commit
 
 public class HelloApplication extends Application {
     @Override
