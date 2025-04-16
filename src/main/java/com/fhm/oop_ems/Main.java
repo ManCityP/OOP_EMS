c217d1f50bc017910f667fb51009485e5c04c4b1
ManCityP <mohamed.a.eltaie@gmail.com>
2025-04-16 20:45:14 +0200

    Update and Test TimeRange class

--- src/main/java/com/fhm/oop_ems/Main.java
+++ src/main/java/com/fhm/oop_ems/Main.java
@@ -3,17 +3,26 @@ package com.fhm.oop_ems;
 import p1.Day;
 import p1.TimeRange;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Map<Day, TimeRange> map = new LinkedHashMap<>();
-        map.put(Day.SATURDAY, new TimeRange(6.25, 17));
-        map.put(Day.MONDAY, new TimeRange(9, 21.57));
-        map.put(Day.TUESDAY, new TimeRange("9:11", "19:12"));
-        map.put(Day.WEDNESDAY, new TimeRange("9:30", "17:15"));
-        map.put(Day.THURSDAY, new TimeRange("1:30", "12:30"));
-        System.out.println(map.toString().replace("=", " --> ").replace(", ", "\n").replaceAll("[{}]", ""));
+        Map<Day, ArrayList<TimeRange>> map = new LinkedHashMap<>();
+        map.put(Day.SATURDAY, new ArrayList<>(List.of(new TimeRange(6.25, 17))));
+        map.put(Day.MONDAY, new ArrayList<>(List.of(new TimeRange(9, 21.57))));
+        map.put(Day.TUESDAY, new ArrayList<>(List.of(new TimeRange("9:11", "19:12"))));
+        map.put(Day.WEDNESDAY, new ArrayList<>(List.of(new TimeRange("9:30", "17:15"))));
+        map.put(Day.THURSDAY, new ArrayList<>(List.of(new TimeRange("12:30", "18:30"), new TimeRange("22:30", "00:30"))));
+        System.out.println(map.toString().replace("=", " --> "));
+        if(map.get(Day.THURSDAY).getFirst().Contains(new TimeRange("15:15" , "17:30"))) {
+            ArrayList<TimeRange> updated = map.get(Day.THURSDAY).getFirst().Remove(new TimeRange("15:15", "17:30"));
+            map.get(Day.THURSDAY).removeFirst();
+            map.get(Day.THURSDAY).addFirst(updated.getLast());
+            map.get(Day.THURSDAY).addFirst(updated.getFirst());
+        }
+        System.out.println(map.toString().replace("=", " --> "));
     }
 }
--- src/main/java/p1/TimeRange.java
+++ src/main/java/p1/TimeRange.java
@@ -1,12 +1,14 @@
 package p1;
 
+import java.util.ArrayList;
+
 public class TimeRange {
     double start;
     double end;
     String range;
 
     public TimeRange(double start, double end) throws Exception {
-        if(start >= end || start < 0 || end >= 24)
+        if(start >= 24 || end < 0 || start < 0 || end >= 24)
             throw new Exception("Invalid time range!");
         this.start = start;
         this.end = end;
@@ -27,12 +29,30 @@ public class TimeRange {
         return String.format("%02d:%02d", Math.round(Math.floor(time)), Math.round((time-Math.floor(time))*60));
     }
 
+    public boolean Contains(TimeRange timeRange) {
+        return (timeRange.start >= this.start && timeRange.end <= this.end);
+    }
+
+    public ArrayList<TimeRange> Remove(TimeRange timeRange) throws Exception {
+        ArrayList<TimeRange> newRange = new ArrayList<>();
+        if (timeRange.start > this.start)
+            newRange.add(new TimeRange(this.start, timeRange.start));
+        if (timeRange.end < this.end)
+            newRange.add(new TimeRange(timeRange.end, this.end));
+        return newRange;
+    }
+
     public static double Convert(String time) throws Exception {
         String[] t = time.split(":");
         double d1 = Double.parseDouble(t[0]);
         if (d1 < 0 || d1 >= 24)
             throw new Exception("Invalid time!");
-        return d1 + Double.parseDouble(t[1])/60.0;
+        double d2 = Double.parseDouble(t[1]);
+        if (d2 < 0 || d2 >= 60)
+            throw new Exception("Invalid time!");
+        if (d1 > Math.floor(d1) || d2 > Math.floor(d2))
+            throw new Exception("Invalid time");
+        return d1 + (d2/60.0);
     }
 
     public boolean InRange(double time) {
