7a30fbf4c6345fc74383b8a990e10d4c79b8e25a
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-04-16 11:07:10 +0200

    fuck you ya mohy

--- src/main/java/p2/Organizer.java
+++ src/main/java/p2/Organizer.java
@@ -1,4 +1,11 @@
 package p2;
 
+import p3.Wallet;
+
 public class Organizer {
+    Wallet wallet;
+
+    public void EditWalletBalance(double amount){
+        this.wallet.EditBalance(amount);
+    }
 }
--- src/main/java/p3/Wallet.java
+++ src/main/java/p3/Wallet.java
@@ -6,18 +6,23 @@ import p2.Organizer;
 public class Wallet {
     double balance;
     int walletNumber;
+
     Wallet(){}
     Wallet(double balance,int walletNumber){
         this.balance = balance;
         this.walletNumber = walletNumber;
     }
 
-    void PurchaseEvent(Event event, Organizer organizer) throws Exception{
+    public void EditBalance(double amount){
+        this.balance += amount;
+    }
+
+    public void PurchaseEvent(Event event, Organizer organizer) throws Exception{
         if(this.balance < event.GetPrice())
             throw new Exception("Invalid balance");
         else{
             this.balance -= event.GetPrice();
-
+            organizer.EditWalletBalance(event.GetPrice());
         }
     }
 }
