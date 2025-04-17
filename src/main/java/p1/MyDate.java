a6188fd2df98f560a4388bc2490ab2ba5334f1ee
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-04-17 17:25:51 +0200

    love you ya mohy

---
+++ src/main/java/p1/MyDate.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class MyDate {
+
+    int day,month,year;
+
+    MyDate(int day,int month, int year){}
+    MyDate(String date){}
+
+    public int GetDay(){}
+    public int GetMonth(){}
+    public int GetYear(){}
+
+    @Override
+    public String toString(){}
+
+    @Override
+    public boolean equals(){}
+}
