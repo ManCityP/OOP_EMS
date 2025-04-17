e471cabc73074dcffc3b69e5abb75292b908c9f7
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-04-17 17:52:51 +0200

    love you ya mohy

--- src/main/java/p1/MyDate.java
+++ src/main/java/p1/MyDate.java
@@ -4,16 +4,43 @@ public class MyDate {
 
     int day,month,year;
 
-    MyDate(int day,int month, int year){}
-    MyDate(String date){}
+    public MyDate(int day,int month, int year) throws Exception {
+        if (day <= 0 || month <= 0 || year <= 0)
+            throw new Exception("Invalid date");
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    public MyDate(String date) throws Exception {
+        String[] d = date.split("/");
+        if (Integer.parseInt(d[0].trim()) <= 0)
+            throw new Exception("Invalid date");
+        this.day = Integer.parseInt(d[0].trim());
+        if (Integer.parseInt(d[1].trim()) <= 0)
+            throw new Exception("Invalid date");
+        this.month = Integer.parseInt(d[1].trim());
+        if (Integer.parseInt(d[2].trim()) <= 0)
+            throw new Exception("Invalid date");
+        this.year = Integer.parseInt(d[2].trim());
+    }
 
-    public int GetDay(){}
-    public int GetMonth(){}
-    public int GetYear(){}
+    public int GetDay(){
+        return this.day;
+    }
+    public int GetMonth(){
+        return this.month;
+    }
+    public int GetYear(){
+        return this.year;
+    }
 
     @Override
-    public String toString(){}
+    public String toString(){
+        return this.day + "/" + this.month + "/" + this.year;
+    }
 
     @Override
-    public boolean equals(){}
+    public boolean equals(Object date){
+        return ( (date instanceof MyDate) && this.day == ((MyDate)date).day && this.month == ((MyDate)date).month && this.year == ((MyDate)date).year);
+    }
 }
