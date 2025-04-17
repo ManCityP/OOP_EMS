4cc46dc26488b765bc89d9da12aeaa066424f3f0
Fris233 <baefris@gmail.com>
2025-04-17 20:29:56 +0200

    DISCLAIMER: CODE MAKTOOB 3LA SEYAMMMMMMM

--- src/main/java/p1/Room.java
+++ src/main/java/p1/Room.java
@@ -4,8 +4,10 @@ public class Room {
     //static int numberOfRooms;
     Hours hours;
     boolean reserved;
-
-
+int id;
+public int GetID(){
+    return this.id;
+}
     Room(){
         //numberOfRooms++;
     };
