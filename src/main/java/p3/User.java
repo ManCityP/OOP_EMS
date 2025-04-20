64c02549ff2e1a50cd41a66b9e18ab3cb7b0a2bd
ManCityP <mohamed.a.eltaie@gmail.com>
2025-04-20 10:26:17 +0200

    User details exception handling

--- src/main/java/p3/User.java
+++ src/main/java/p3/User.java
@@ -22,14 +22,23 @@ public abstract class User {
         // numberOfUsers++;
     }
 
-    protected User(String username, String email, String password, MyDate dob, Gender gender){
+    protected User(String username, String email, String password, MyDate dob, Gender gender) throws Exception {
+
+        if(!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"))
+            throw new Exception("Invalid Email format");
+        if(email.length() > 128)
+            throw new Exception("Email must be at most 128 characters");
+        if(username.isEmpty() || username.length() > 32)
+            throw new Exception("Username must be 1-32 characters");
+        if(password.length() < 8 || password.length() > 32)
+            throw new Exception("Password must be 8-32 characters long");
+        //TODO if we want strict password rules.
+
         this.username=username;
         this.email = email;
         this.password=password;
         this.dob=dob;
         this.gender=gender;
-
-        // numberOfUsers++;
     }
 }
 
