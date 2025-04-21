ee2421cdfdb309ee617f6b1ad94514c93d219d72
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-04-21 17:47:18 +0200

    some code

--- src/main/java/p2/Organizer.java
+++ src/main/java/p2/Organizer.java
@@ -45,8 +45,9 @@ public class Organizer extends User {
         return null;
     }
 
-    public void CreateEvent(double price, Room room,String eventTitle, Category category, MyDate date, TimeRange timeRange) throws Exception {
+    public void CreateEvent(double price, Room room, String eventTitle, Category category, MyDate date, TimeRange timeRange) throws Exception {
         room.ReserveEvent(new Event(this, eventTitle, 0, price, room.GetID(), category, date, timeRange));
+        eventTitle = eventTitle.replace("'","\\'");
         Database.Execute(String.format("INSERT INTO event (username, price, category, room_id, date, time_range, title) " +
                                        "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')",
                                        username, price, category.toString(), room.GetID(), date.toString(), timeRange.toString(), eventTitle));
