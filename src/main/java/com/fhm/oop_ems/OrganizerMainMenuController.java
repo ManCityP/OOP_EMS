f69b064002cc53085c27949695ca28f5e525e3e4
AhmedHesham102 <ahmedhesham1626@gmail.com>
2025-05-01 23:29:18 +0200

    Organizer menu code modified

--- src/main/java/com/fhm/oop_ems/OrganizerMainMenuController.java
+++ src/main/java/com/fhm/oop_ems/OrganizerMainMenuController.java
@@ -1,11 +1,29 @@
 package com.fhm.oop_ems;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OrganizerMainMenuController {
+
+    @FXML
+    private Label createEventButton;
+    @FXML
+    private Label eventsButton;
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private Label myWalletButton;
+    @FXML
+    private Label profileButton;
 
     @FXML
     private void ButtonHovered(MouseEvent event) {
@@ -29,5 +47,14 @@ public class OrganizerMainMenuController {
     private void LabelNotHovered(MouseEvent event) {
         Label label = (Label) event.getSource();
         label.setStyle("-fx-underline: false;");
+    }
+
+    @FXML
+    private void LogoutPressed() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)logoutButton.getScene().getWindow();
+        stage.setScene(scene);
     }
 }
